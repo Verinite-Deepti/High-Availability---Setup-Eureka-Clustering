a7a4ee683c1dcdc51c9d76a0c380ad85a533f0c6
Deeptiwani <Deeptiwani@host.docker.internal>
2024-08-28 18:15:48 +0530

    Initial Commit.

---
+++ eureka-clustering-server1/src/test/java/com/eurekaclustering1/EurekaClusteringServerApplication1Tests.java
@@ -0,0 +1,13 @@
+package com.eurekaclustering1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClusteringServerApplication1Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
